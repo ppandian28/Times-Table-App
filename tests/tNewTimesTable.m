7e521c3d08baa417637e263729a03046959d4b89
ppandian28 <54954089+ppandian28@users.noreply.github.com>
2020-11-12 10:35:39 -0500

    Update tNewTimesTable.m

--- tests/tNewTimesTable.m
+++ tests/tNewTimesTable.m
@@ -21,7 +21,7 @@ classdef tNewTimesTable < matlab.unittest.TestCase
             
             % Times table expects a string for the currentTimesTable
             testCase.gameObject.setNewTimesTable(num2str(newTable));
-            testCase.verifyTrue(testCase.gameObject.CurrentTimesTable, newTable);            
+            testCase.verifyEqual(testCase.gameObject.CurrentTimesTable, newTable);            
         end
     end
     
