9d6f3b9556a41df2e0d5a21d12070eeea58b8b35
ppandian28 <54954089+ppandian28@users.noreply.github.com>
2020-11-12 10:30:48 -0500

    Update tCurrentQuestion.m

--- tests/tCurrentQuestion.m
+++ tests/tCurrentQuestion.m
@@ -20,8 +20,8 @@ classdef tCurrentQuestion < matlab.unittest.TestCase
                 % Set a new question:
                 testCase.gameObject.setNewQuestion;
                 % Confirm within expected range:
-                testCase.verifyGreaterThanOrEqual( testCase.gameObject.CurrentQuestion, 11);
-                testCase.verifyLessThanOrEqual( testCase.gameObject.CurrentQuestion, 121);
+                testCase.verifyGreaterThanOrEqual( testCase.gameObject.CurrentQuestion, 1);
+                testCase.verifyLessThanOrEqual( testCase.gameObject.CurrentQuestion, 12);
             end
         end
     end
